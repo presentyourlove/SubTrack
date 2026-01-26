210d0e8109e16e25151ef27004a589845d64ca08
presentyourlove <jasonwth0928@gmail.com>
2026-01-26 12:39:44 +0800

    fix: 調整 eslint 設定，在測試檔案中禁用 no-unused-vars 規則

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
         '@typescript-eslint/no-require-imports': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
         'react/display-name': 'off',
       },
     },
