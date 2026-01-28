4efb9351ee022cf666318427f24ee1c2e01aa158
presentyourlove <jasonwth0928@gmail.com>
2026-01-28 16:09:38 +0800

    style: fix lint errors in WebGlobalStyles

--- src/components/WebGlobalStyles.tsx
+++ src/components/WebGlobalStyles.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { Platform } from 'react-native';
 
 export function WebGlobalStyles() {
-    if (Platform.OS !== 'web') return null;
+  if (Platform.OS !== 'web') return null;
 
-    return (
-        <style type="text/css">{`
+  return (
+    <style type="text/css">{`
       @font-face {
         font-family: 'MaterialCommunityIcons';
         src: url(${require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf')}) format('truetype');
@@ -13,15 +14,15 @@ export function WebGlobalStyles() {
         font-family: 'Ionicons';
         src: url(${require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf')}) format('truetype');
       }
-      
+
       body {
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
       }
-      
+
       /* 確保輸入框和按鈕也繼承字體 */
       input, button, select, textarea {
         font-family: inherit;
       }
     `}</style>
-    );
+  );
 }
