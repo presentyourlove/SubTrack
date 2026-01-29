c1c1efa81672c9d2a4b788ecb1b37443186340fe
jasonwth <jasonwth0928@gmail.com>
2026-01-29 22:28:45 +0800

    test: fix Jest config for Skia Web modules
    - Add @shopify/react-native-skia to transformIgnorePatterns
    - Fixes SyntaxError: Unexpected token 'export' during tests

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|firebase|@firebase|expo-.*|react-native-reanimated|react-native-worklets)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|firebase|@firebase|expo-.*|react-native-reanimated|react-native-worklets|@shopify/react-native-skia)',
   ],
   moduleNameMapper: {
     '^expo$': '<rootDir>/__mocks__/expo.js',
